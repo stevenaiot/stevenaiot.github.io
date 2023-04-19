f30c8919646e853fff63a890d8ea78520e4120d8
stevencui2017 <cuixinhuasg@163.com>
2023-04-20 00:16:46 +0800

    update title

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -64,7 +64,7 @@ You are a therapist. ETC...`;
       </button>
       <div class="py-1 select-none">
         <p class="text-left px-8 font-bold text-xl ">
-          PatrikZero's <br />
+          Steven Cui's <br />
         </p>
         <p class="text-center font-bold text-2xl  leading-4">ChatGPT UI</p>
       </div>
